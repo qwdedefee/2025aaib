958abb4cb17b1640b8b2bf1210ef35420b45e02d
qwdedefee <gary23381505@gmail.com>
2025-03-05 11:48:05 +0800

    week03

---
+++ week03_3.cpp
@@ -0,0 +1,14 @@
+///week03-3.cpp
+#include <stdio.h>
+int main()
+{///從0開始，不超過<15，就跑15次
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",i);
+    }///電腦的for迴圈，start at "0"
+    printf("\n");
+    for(int i=1;i<=5;i++)
+    {
+        printf("%d ",i);
+    }///人類的的for迴圈，start at "1"
+}
