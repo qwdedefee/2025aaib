65a8967a9c12b45d77dcc0a217a7f94bf242add4
qwdedefee <gary23381505@gmail.com>
2025-05-07 11:28:56 +0800

    week12

---
+++ week12-2.cpp
@@ -0,0 +1,15 @@
+///week12-2.cpp
+#include <iostream>
+using namespace std;
+int prime(int n){
+	int b=0;
+	for(int i=2;i<n;i++)
+		if(n%i==0)
+			return 0;
+	return 1;
+}
+int main(){
+  int n;cin>>n;
+  cout<<"["<<prime(n)<<"]";
+  return 0;
+}
---
+++ week12_1_happy_bubble_3D_part1/week12_1_happy_bubble_3D_part1.pde
@@ -0,0 +1,11 @@
+//week12_1_happy_bubble_3D_part1
+void setup(){
+  size(300,300,P3D);
+}
+void draw(){
+  background(255);
+  pushMatrix();
+  translate(mouseX,mouseY);
+  sphere(60);
+  popMatrix();
+}
